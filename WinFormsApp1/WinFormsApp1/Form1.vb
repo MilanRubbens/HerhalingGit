9b4327728111e72587a81ddef5c874f9c49ff9df
Milan Rubbens <milan.rubbens7@gmail.com>
2023-01-26 15:02:15 +0100

    HerhalingGit

---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim nummer As Integer = 7
+        Dim nummer2 As Integer = 9
+        Dim som = nummer + nummer2
+        MsgBox("De som van de twee cijfers is " & som)
+
+    End Sub
+End Class
