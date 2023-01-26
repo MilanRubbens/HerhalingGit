11c5c2217074ceb38f8800fae82ecf456e8b9a58
Yason Prüfer <yason.pr@gmail.com>
2023-01-26 15:08:23 +0100

    hardcoded cijfers vervangen door een inputbox

--- WinFormsApp1/WinFormsApp1/Form1.vb
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim nummer As Integer = 7
-        Dim nummer2 As Integer = 9
+        Dim nummer As Integer = InputBox("Geef de eerste nummer in:") 'inputbox ipv hard coded cijfers pull nr #1
+        Dim nummer2 As Integer = InputBox("Geef de tweede nummer in:")
         Dim som = nummer + nummer2
         MsgBox("De som van de twee cijfers is " & som)
 
